24001bdd3b3f7af7104255b93a04ee102d2d96da
LS <LS.com>
2026-01-04 20:49:08 +0000

    Added new Core.

---
+++ ATAP/Core/DataPoint.cs
@@ -0,0 +1,12 @@
+﻿// <copyright file="DataPoint.cs" company="LarissaStupar1974">
+// Copyright (c) LarissaStupar1974. All rights reserved.
+// </copyright>
+
+namespace Core;
+
+/// <summary>
+/// Time and value.
+/// </summary>
+/// <param name="TimeStamp">Time stamp.</param>
+/// <param name="Value">Value.</param>
+public record DataPoint(DateTime TimeStamp, double Value);
---
+++ ATAP/Core/TimeSeries.cs
@@ -0,0 +1,11 @@
+﻿// <copyright file="TimeSeries.cs" company="LarissaStupar1974">
+// Copyright (c) LarissaStupar1974. All rights reserved.
+// </copyright>
+
+namespace Core;
+
+/// <summary>
+/// Time series class.
+/// </summary>
+/// <param name="Values">Data points.</param>
+internal record TimeSeries(IReadOnlyList<DataPoint> Values);
