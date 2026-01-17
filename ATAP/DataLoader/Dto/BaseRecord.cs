6bdee4035909413181cef91c6b0d7792f90dab12
LS <LS.com>
2026-01-17 18:22:29 +0000

    stash

---
+++ ATAP/DataLoader/Dto/BaseRecord.cs
@@ -0,0 +1,13 @@
+﻿// <copyright file="BaseRecord.cs" company="LarissaStupar1974">
+// Copyright (c) LarissaStupar1974. All rights reserved.
+// </copyright>
+
+namespace DataLoader.Dto;
+
+public record BaseRecord
+{
+    /// <summary>
+    /// Gets the Symbol.
+    /// </summary>
+    public required string Symbol { get; init; }
+}
---
+++ ATAP/DataMangler/Extensions.cs
@@ -0,0 +1,19 @@
+﻿// <copyright file="Extensions.cs" company="LarissaStupar1974">
+// Copyright (c) LarissaStupar1974. All rights reserved.
+// </copyright>
+
+namespace DataLoader;
+
+using System.Collections.Generic;
+using System.Linq;
+using DataLoader.Dto;
+
+public static class Extensions
+{
+    public static IReadOnlyDictionary<string, IReadOnlyList<T>> GroupBySymbol<T>(this IReadOnlyList<T> records)
+        where T : BaseRecord
+    {
+        Dictionary<string, List<T>> grouped = records.GroupBy(r => r.Symbol).ToDictionary(l => l.Key, l => l.ToList());
+        return (IReadOnlyDictionary<string, IReadOnlyList<T>>)grouped;
+    }
+}
